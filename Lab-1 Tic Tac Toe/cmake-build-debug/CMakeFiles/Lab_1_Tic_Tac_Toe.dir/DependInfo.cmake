3af2ce19b27457a70a648a93bf52d38f6ccfbec4
RoscoeTheDog <54086262+RoscoeTheDog@users.noreply.github.com>
2020-01-14 13:48:13 -0800

    Update
    Reduced for loops for validation checking.
    Fixed bugs related to the change.

--- Lab-1 Tic Tac Toe/cmake-build-debug/CMakeFiles/Lab_1_Tic_Tac_Toe.dir/DependInfo.cmake
+++ Lab-1 Tic Tac Toe/cmake-build-debug/CMakeFiles/Lab_1_Tic_Tac_Toe.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Admin/Documents/Github/C++/CS162C+/Lab-1 Tic Tac Toe/main.cpp" "C:/Users/Admin/Documents/Github/C++/CS162C+/Lab-1 Tic Tac Toe/cmake-build-debug/CMakeFiles/Lab_1_Tic_Tac_Toe.dir/main.cpp.obj"
+  "C:/Users/Admin/Documents/GitHub/C++/CS162C-/Lab-1 Tic Tac Toe/main.cpp" "C:/Users/Admin/Documents/GitHub/C++/CS162C-/Lab-1 Tic Tac Toe/cmake-build-debug/CMakeFiles/Lab_1_Tic_Tac_Toe.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- Lab-1 Tic Tac Toe/main.cpp
+++ Lab-1 Tic Tac Toe/main.cpp
@@ -2,14 +2,13 @@
 
 using namespace std;
 
-
 bool REPEAT = false;
 
-class TicTacToe
-{
+class TicTacToe {
 private:
     char PLAYER1_SYMBOL = 'X';
     char PLAYER2_SYMBOL = '0';
+    char PLACE_SYMBOL = ' ';
     int WINNER = 0;
 
     int currentPlayer = 1;
@@ -19,23 +18,21 @@ private:
     int playerRow = 0;
 
 public:
-
     // Expose game-state MACRO for main();
     bool WIN = false;
     bool DRAW = false;
 
-    TicTacToe(){
+    TicTacToe() {
 
         // Constructor will Initialize/Populate the board with the defaultEmptySymbol.
-        for (int i=0; i<sizeof(board[i])/sizeof(char); ++i)
-        {
-            for (int j=0; j<sizeof(board[j])/sizeof(char); ++j)
+        for (int i = 0; i < sizeof(board[i]) / sizeof(char); ++i) {
+            for (int j = 0; j < sizeof(board[j]) / sizeof(char); ++j)
                 board[i][j] = defaultEmptySymbol;
         }
 
     }
 
-    void displayInstructions(){
+    void displayInstructions() {
         cout << "Welcome to Tic Tac Toe.\n"
              << "To begin, please enter a column and row number.\n"
              << "Player 1 goes first." << endl;
@@ -43,14 +40,12 @@ public:
         showBoard();
     }
 
-    void showBoard(){
+    void showBoard() {
 
         // Iterate through Columns (Y)
-        for (int i=0; i<sizeof(board[i])/sizeof(char); ++i)
-        {
+        for (int i = 0; i < sizeof(board[i]) / sizeof(char); ++i) {
             // Iterate through Rows (X)
-            for (int j=0; j<sizeof(board[j])/sizeof(char); ++j)
-            {
+            for (int j = 0; j < sizeof(board[j]) / sizeof(char); ++j) {
                 // Display board[column][row] value
                 cout << board[i][j];
             }
@@ -60,210 +55,135 @@ public:
         }
     }
 
-    void getMove(){
+    void getMove() {
 
         // Switch the Filler to be used on the input coordinate
-        switch (currentPlayer)
-        {
+        switch (currentPlayer) {
             case 1:
-                defaultEmptySymbol = 'X'; break;
-
+                PLACE_SYMBOL = 'X';
+                break;
             case 2:
-                defaultEmptySymbol = '0'; break;
+                PLACE_SYMBOL = '0';
+                break;
         }
 
         // Restart move if position on board is already occupied.
-        do
-        {
+        do {
             // Prompt the player for input (X).
             cout << "Player " << currentPlayer << ", enter the desired Column number (1-3)" << endl;
 
             // Validate the input.
-            do
-            {
+            do {
                 cin >> playerColumn;
 
                 // Check to see if valid integer was entered
-                if (cin.fail())
-                {
+                if (cin.fail()) {
                     cout << "Please enter a valid integer number between 1 and 3" << endl;
                     cin.clear();
                     cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 }
 
-                if (playerColumn < 1 || playerColumn > 3 )
+                if (playerColumn < 1 || playerColumn > 3)
                     cout << "Please enter a valid integer number between 1 and 3" << endl;
 
-            }while (playerColumn < 1 || playerColumn > 3 );     // Check to see if between number of rows and columns.
+            } while (playerColumn < 1 || playerColumn > 3);     // Check to see if between number of rows and columns.
 
             // After validation, -1 from player answer so it translates to the 0 indexed array.
-            playerColumn --;
+            playerColumn--;
 
             // Prompt the player for input (Y).
             cout << "Player " << currentPlayer << ", enter the desired Row number (1-3)" << endl;
             // Validate the input.
-            do
-            {
+            do {
                 cin >> playerRow;
 
                 // Check to see if valid integer was entered
-                if (cin.fail())
-                {
+                if (cin.fail()) {
                     cout << "Please enter a valid integer number between 1 and 3" << endl;
                     cin.clear();
                     cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 }
 
-                if (playerRow < 1 || playerRow > 3 )
+                if (playerRow < 1 || playerRow > 3)
                     cout << "Please enter a valid integer number between 1 and 3" << endl;
 
                 // Check to see if between number of rows and columns.
-            }while (playerRow < 1 ||
-                    playerRow > 3 );
+            } while (playerRow < 1 ||
+                     playerRow > 3);
 
             // After validation, -1 from player answer so it translates to the 0 indexed array.
-            playerRow --;
+            playerRow--;
 
             if (board[playerRow][playerColumn] == 'X' || board[playerRow][playerColumn] == '0')
                 cout << "That position has already been used. Please restart your move." << endl;
 
-        }while(board[playerRow][playerColumn] == 'X' || board[playerRow][playerColumn] == '0');
+        } while (board[playerRow][playerColumn] == 'X' || board[playerRow][playerColumn] == '0');
 
         // Finally, store the player's move on the board. (x, y)
-        board[playerRow][playerColumn] = defaultEmptySymbol;
+        board[playerRow][playerColumn] = PLACE_SYMBOL;
 
         cout << endl;
     }
 
-    void checkWin(){
+    void checkWin() {
 
         // var used to check if 3 in a row.
         int validationCounter = 0;
 
         // Validate Method: Horizontal
-        for (int i=0; i<sizeof(board[i])/sizeof(char); ++i) // Iterate through Y position
+        for (int i = 0; i < sizeof(board[i]) / sizeof(char); ++i) // Iterate through Y position
         {
-            // Check for player1 symbol
-            if (board[0][i] == PLAYER1_SYMBOL)
-            {
-                // Iterate through X position
-                for (int j=0; j<sizeof(board[j])/sizeof(char); ++j)
-                    if (board[0][i] == board[j][i])
-                        validationCounter ++;
+            // Iterate through X position
+            for (int j = 0; j < sizeof(board[j]) / sizeof(char); ++j)
+                if (board[0][i] == board[j][i] &&
+                    board[0][i] != defaultEmptySymbol)
+                    validationCounter++;    // Ignore if cell is empty.
 
-                // If 3 in a row, player wins.
-                if (validationCounter == 3)
-                {
-                    WIN = true;
-                    WINNER = 1;
-                }
-                else
-                    validationCounter = 0;
-            }
-                // Check for player2 symbol
-            else if (board[0][i] == PLAYER2_SYMBOL)
-            {
-                // Iterate through X position
-                for (int j=0; j<sizeof(board[j])/sizeof(char); ++j)
-                    if (board[0][i] == board[j][i])
-                        validationCounter ++;
-
-                // If 3 in a row, player wins.
-                if (validationCounter == 3)
-                {
-                    WIN = true;
-                    WINNER = 2;
-                }
-                    // Reset validation counter.
-                else
-                    validationCounter = 0;
-            }
+            // If 3 in a row, player wins.
+            if (validationCounter == 3) {
+                WIN = true;
+                WINNER = currentPlayer;
+            } else
+                validationCounter = 0;
         }
 
         // Validation Method: Vertical.
         validationCounter = 0;  // Reset validationCounter.
-        for (int i=0; i<sizeof(board[i])/sizeof(char); ++i) // Iterate through X position.
+        for (int i = 0; i < sizeof(board[i]) / sizeof(char); ++i) // Iterate through X position.
         {
-            // Check for player1 symbol.
-            if (board[i][0] == PLAYER1_SYMBOL)
-            {
-                // Iterate through Y position.
-                for (int j=0; j<sizeof(board[j])/sizeof(char); ++j)
-                    if (board[i][0] == board[i][j])
-                        validationCounter ++;
+            // Iterate through Y position.
+            for (int j = 0; j < sizeof(board[j]) / sizeof(char); ++j)
+                if (board[i][0] == board[i][j] &&
+                    board[i][0] != defaultEmptySymbol)  // Ignore empty cells.
+                    validationCounter++;
 
-                // If 3 in a row, player wins.
-                if (validationCounter == 3)
-                {
-                    WIN = true;
-                    WINNER = 1;
-                }
-                    // Reset validation counter.
-                else
-                    validationCounter = 0;
-            }
-
-                // Check for player2 symbol.
-            else if(board[i][0] == PLAYER2_SYMBOL)
-            {
-                // Iterate through Y position.
-                for (int j=0; j<sizeof(board[j])/sizeof(char); ++j)
-                    if (board[i][0] == board[i][j])
-                        validationCounter ++;
-
-                // If 3 in a row, player wins.
-                if (validationCounter == 3)
-                {
-                    WIN = true;
-                    WINNER = 2;
-                }
-                    // Reset validation counter.
-                else
-                    validationCounter = 0;
-            }
+            // If 3 in a row, player wins.
+            if (validationCounter == 3) {
+                WIN = true;
+                WINNER = currentPlayer;
+            } else
+                // Reset validation counter.
+                validationCounter = 0;
         }
 
         // Validation Method: Diagonal.
         validationCounter = 0;  // Reset validationCounter.
 
         // Check to make sure we are not validating empty spaces in a row.
-        if (board[0][0] != '.')
-        {
+        if (board[0][0] != defaultEmptySymbol) {
             // DIRECTION: TOP LEFT TO BOTTOM RIGHT.
-            if (board[0][0] == PLAYER1_SYMBOL)
-            {
-                // 0,0 increment by (+) 1 down -> (+) 1 right
-                for (int i = 0; i < 3; ++i)
-                    if (board[0][0] == board[i][i] && board[i][i] != '.')
-                        validationCounter++;
+            // 0,0 increment by (+) 1 down -> (+) 1 right
+            for (int i = 0; i < 3; ++i)
+                if (board[0][0] == board[i][i] &&
+                    board[i][i] != defaultEmptySymbol)
+                    validationCounter++;
 
-                // If 3 in a row, player wins.
-                if (validationCounter == 3)
-                {
-                    WIN = true;
-                    WINNER = 1;
-                }
-                else
-                    validationCounter = 0;
-            }
-
-            else if (board[0][0] == PLAYER2_SYMBOL)
-            {
-                // TOP LEFT TO BOTTOM RIGHT
-                // 0,0 increment by (+) 1 down -> (+) 1 right
-                for (int i = 0; i < 3; ++i)
-                    if (board[0][0] == board[i][i] && board[i][i] != '.')
-                        validationCounter++;
-
-                // If 3 in a row, player wins.
-                if (validationCounter == 3)
-                {
-                    WIN = true;
-                    WINNER = 2;
-                }
-                else
-                    validationCounter = 0;
-            }
+            // If 3 in a row, player wins.
+            if (validationCounter == 3) {
+                WIN = true;
+                WINNER = currentPlayer;
+            } else
+                validationCounter = 0;
         }
 
         // DIRECTION: BOTTOM LEFT TO top RIGHT
@@ -272,42 +192,22 @@ public:
         int y = 2;
 
         // Check to make sure we are not validating empty spaces in a row.
-        if (board[0][2] != '.')
-        {
+        if (board[0][2] != defaultEmptySymbol) {
             // 0, 2 increment by (-) 1 up -> (+1) right
-            for (int i=0; i<3; ++i)
-                if(board[0][2] == board[x++][y--])
-                    validationCounter ++;
+            for (int i = 0; i < 3; ++i)
+                if (board[0][2] == board[x++][y--])
+                    validationCounter++;
 
             // Check if 3 in a row AND last value validated was player 1 symbol
-            if (validationCounter == 3 && board[x][y] == PLAYER1_SYMBOL)
-            {
+            if (validationCounter == 3) {
                 WIN = true;
-                WINNER = 1;
-            }
-                // Check if 3 in a row AND last value validated was player 2 symbol
-            else if (validationCounter == 3 && board[x][y] == PLAYER2_SYMBOL)
-            {
-                WIN = true;
-                WINNER = 2;
-            }
-            else
+                WINNER = currentPlayer;
+            } else
                 validationCounter = 0;
         }
 
         if (WIN)
             cout << "Congratulations Player " << WINNER << ", you win!" << endl;
-
-// TBD: return 4 corners of the board stored as an array?
-//
-//    int corners[4] = {board[0][0], board[3][0], board[0][3], board[3][3]};
-//
-//    for (int i=0; i<4; ++i)
-//    {
-//        // Validation Method: Diagonal.
-//        if (corners[i] == PLAYER1_SYMBOL)
-//    }
-
     }
 
     void checkDraw() {
@@ -316,57 +216,48 @@ public:
         int validated = 0;
 
         // Iterate through X
-        for (int i = 0; i < sizeof(board[i]) / sizeof(char); ++i)
-        {
+        for (int i = 0; i < sizeof(board[i]) / sizeof(char); ++i) {
             // Iterate through Y
             for (int j = 0; j < sizeof(board[j]) / sizeof(char); ++j)
-                if (board[i][j] != '.')
+                if (board[i][j] != defaultEmptySymbol)
                     validated++;
         }
 
         // If all cells are used, we know the players have hit a draw.
-        if (validated == 9)
-        {
+        if (validated == 9) {
             cout << "The game ended in a draw." << endl;
             DRAW = true;
-        }
-        else
+        } else
             DRAW = false;
 
     }
 
-    void changePlayer()
-    {
+    void changePlayer() {
         if (currentPlayer == 1)
             currentPlayer = 2;
         else if (currentPlayer == 2)
             currentPlayer = 1;
     }
 
-    void promptNewGame()
-    {
+    void static promptNewGame() {
         char userInput = ' ';
 
         cout << "Would you like to start a new game? (y/n)" << endl;
 
-        do
-        {
+        do {
             cin >> userInput;
             userInput = toupper(userInput);
-
-            cout << userInput << endl;
 
             if (userInput != 'Y' && userInput != 'N')
                 cout << "Please enter 'y' or 'n' to restart or quit." << endl;
 
-        }while (userInput != 'Y' && userInput != 'N');
+        } while (userInput != 'Y' && userInput != 'N');
 
         if (userInput == 'Y')
             REPEAT = true;
-        else
+        else if (userInput == 'N')
             REPEAT = false;
     }
-
 };
 
 int main() {
@@ -392,9 +283,9 @@ int main() {
             Game.changePlayer();
         } while (!Game.WIN && !Game.DRAW);
 
-        Game.promptNewGame();
+        TicTacToe::promptNewGame();
 
-    // display final board and congratulate winner
+        // display final board and congratulate winner
     } while (REPEAT);
 
     return 0;
