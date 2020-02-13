beb98665601f7decb9b6d467505d1e40847e24b2
RoscoeTheDog <54086262+RoscoeTheDog@users.noreply.github.com>
2020-02-12 20:45:10 -0800

    Lab 5 Redo

---
+++ Lab-5 Redo/cmake-build-debug/CMakeFiles/Lab_5_Redo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_5_Redo.dir/main.cpp.obj"
+  "Lab_5_Redo.exe"
+  "Lab_5_Redo.exe.manifest"
+  "Lab_5_Redo.pdb"
+  "libLab_5_Redo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_5_Redo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab-5 Redo/main.cpp
@@ -0,0 +1,12 @@
+#include "MemoryGame.h"
+
+
+int main() {
+    MemoryGame Game;
+
+    Game.hideCards();
+    Game.displayBoard();
+
+    return 0;
+}
+
